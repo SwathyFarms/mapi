0a718da1f15fb63ddf7a44c7abf3312d75f8d2e3
Athul Raj <mail@athulraj.com>
2022-05-08 12:29:09 +0530

    fix

--- assets/app.js
+++ assets/app.js
@@ -1,57 +1,120 @@
 let map;
+var farm_main_plot_polygon;
 
 function initMap() {
-	const sf_cords = {
+
+	const house_cords = {
 		lat: 8.77721105135935,
 		lng: 77.46167218773766
 	}
+	const farm_center_cords = {
+		lat: 8.776286,
+		lng: 77.462873,
+	}
 	const map = new google.maps.Map(document.getElementById("map"), {
-		center: sf_cords,
+		center: farm_center_cords,
 		zoom: 18,
 		mapTypeId: 'satellite'
 	});
 	const marker = new google.maps.Marker({
-		position: sf_cords,
+		position: house_cords,
 		map: map,
+		title: "Swathy Farms",
 	});
-	const triangleCoords = [
-		{ lat: 8.779172, lng: 77.463019 }, // 8.779172, 77.463019
-		{ lat: 8.779298, lng: 77.462482 }, // 8.779298, 77.462482
-		{ lat: 8.777510, lng: 77.460259 }, // 8.777510, 77.460259
-		{ lat: 8.778667, lng: 77.459900 }, // 8.778667, 77.459900
-		{ lat: 8.778761, lng: 77.459392 }, // 8.778761, 77.459392
-		{ lat: 8.777548, lng: 77.459376 }, // 8.777548, 77.459376
-		{ lat: 8.777204, lng: 77.459970 }, // 8.777204, 77.459970
-		{ lat: 8.777056, lng: 77.460041 }, // 8.777056, 77.460041
-		{ lat: 8.776586, lng: 77.459898 }, // 8.776586, 77.459898
-		{ lat: 8.776281, lng: 77.459820 }, // 8.776281, 77.459820
-		{ lat: 8.775486, lng: 77.460054 }, // 8.775486, 77.460054
-		{ lat: 8.774713, lng: 77.460420 }, // 8.774713, 77.460420
-		{ lat: 8.774511, lng: 77.460497 }, // 8.774511, 77.460497
-		{ lat: 8.773520, lng: 77.461137 }, // 8.773520, 77.461137
-		{ lat: 8.772199, lng: 77.461967 }, // 8.772199, 77.461967
-		{ lat: 8.772240, lng: 77.462381 }, // 8.772240, 77.462381
-		{ lat: 8.772411, lng: 77.462311 }, // 8.772411, 77.462311
-		{ lat: 8.772641, lng: 77.462373 }, // 8.772641, 77.462373
-		{ lat: 8.773213, lng: 77.462779 }, // 8.773213, 77.462779
-		{ lat: 8.772983, lng: 77.463514 }, // 8.772983, 77.463514
-		{ lat: 8.775112, lng: 77.464441 }, // 8.775112, 77.464441
-		{ lat: 8.774711, lng: 77.465395 }, // 8.774711, 77.465395
-		{ lat: 8.776709, lng: 77.465714 }, // 8.776709, 77.465714
-		{ lat: 8.777073, lng: 77.464484 }, // 8.777073, 77.464484
-		{ lat: 8.777533, lng: 77.463892 }, // 8.777533, 77.463892
-
-	  ];
-	const bermudaTriangle = new google.maps.Polygon({
-		paths: triangleCoords,
-		strokeColor: "#00FF00",
+	farm_main_plot_cords = [
+		{ lat: 8.7792741,	lng: 77.4629707},
+		{ lat: 8.7793431,	lng: 77.462478},
+		{ lat: 8.7790412,	lng: 77.4618465},
+		{ lat: 8.7785458,	lng: 77.461278},
+		{ lat: 8.777510,	lng: 77.460259},
+		{ lat: 8.7776061,	lng: 77.4598985},
+		{ lat: 8.7781339,	lng: 77.4599167},
+		{ lat: 8.778667,	lng: 77.4599},
+		{ lat: 8.778761,	lng: 77.459392},
+		{ lat: 8.7778585,	lng: 77.4594066},
+		{ lat: 8.7777379,	lng: 77.459397},
+		{ lat: 8.7776824,	lng: 77.4593913},
+		{ lat: 8.7776164,	lng: 77.4593772},
+		{ lat: 8.777564,	lng: 77.4593677},
+		{ lat: 8.7775306,	lng: 77.4593622},
+		{ lat: 8.7775117,	lng: 77.4593726},
+		{ lat: 8.7774897,	lng: 77.4593881},
+		{ lat: 8.7772428,	lng: 77.4599479},
+		{ lat: 8.7771974,	lng: 77.4599646},
+		{ lat: 8.7771769,	lng: 77.4599844},
+		{ lat: 8.7771499,	lng: 77.4599881},
+		{ lat: 8.7770507,	lng: 77.4600222},
+		{ lat: 8.776586,	lng: 77.4599128},
+		{ lat: 8.7762823,	lng: 77.4597959},
+		{ lat: 8.7761008,	lng: 77.4598225},
+		{ lat: 8.7759193,	lng: 77.4598545},
+		{ lat: 8.7757252,	lng: 77.459908},
+		{ lat: 8.7755629,	lng: 77.4599802},
+		{ lat: 8.7753179,	lng: 77.4601083},
+		{ lat: 8.7750862,	lng: 77.4602497},
+		{ lat: 8.7748877,	lng: 77.4603657},
+		{ lat: 8.774713,	lng: 77.46042},
+		{ lat: 8.774557,	lng: 77.4604598},
+		{ lat: 8.7744036,	lng: 77.4605131},
+		{ lat: 8.7738412,	lng: 77.4608505},
+		{ lat: 8.7736932,	lng: 77.4610179},
+		{ lat: 8.7735266,	lng: 77.4611531},
+		{ lat: 8.7724789,	lng: 77.4616456},
+		{ lat: 8.7724372,	lng: 77.4617103},
+		{ lat: 8.7721816,	lng: 77.4619107},
+		{ lat: 8.7722507,	lng: 77.4620059},
+		{ lat: 8.7722466,	lng: 77.4622402},
+		{ lat: 8.7723056,	lng: 77.4621971},
+		{ lat: 8.7723606,	lng: 77.4621098},
+		{ lat: 8.7724736,	lng: 77.46212},
+		{ lat: 8.772645,	lng: 77.4623341},
+		{ lat: 8.7732289,	lng: 77.4627938},
+		{ lat: 8.772983,	lng: 77.463514},
+		{ lat: 8.7751345,	lng: 77.4644383},
+		{ lat: 8.7748275,	lng: 77.4650916},
+		{ lat: 8.7748001,	lng: 77.465144},
+		{ lat: 8.7747665,	lng: 77.4651657},
+		{ lat: 8.774718,	lng: 77.4651823},
+		{ lat: 8.7746686,	lng: 77.4651914},
+		{ lat: 8.7746201,	lng: 77.4651827},
+		{ lat: 8.7745815,	lng: 77.4652459},
+		{ lat: 8.7747083,	lng: 77.4654258},
+		{ lat: 8.7757026,	lng: 77.4655982},
+		{ lat: 8.7767102,	lng: 77.4657638},
+		{ lat: 8.7767531,	lng: 77.4657409},
+		{ lat: 8.7767938,	lng: 77.4656711},
+		{ lat: 8.7771101,	lng: 77.4645081},
+		{ lat: 8.7775455,	lng: 77.4638782},
+		{ lat: 8.7776178,	lng: 77.4638652}
+	];
+	farm_main_plot_polygon = new google.maps.Polygon({
+		paths: farm_main_plot_cords,
+		strokeColor: "#FF0000",
 		strokeOpacity: 0.8,
 		strokeWeight: 2,
-		fillColor: "#00FF00",
-		fillOpacity: 0.35,
+		fillColor: "#FF0000",
+		fillOpacity: 0.25,
+		// editable: true
 	});
-	
-	bermudaTriangle.setMap(map);
+
+	farm_main_plot_polygon.setMap(map);
+
+	// google.maps.event.addListener(farm_main_plot_polygon, 'set_at', dump_info);
+	// google.maps.event.addListener(farm_main_plot_polygon, 'insert_at', dump_info); 
+	google.maps.event.addListener(farm_main_plot_polygon.getPath(), "insert_at", getPolygonCoords);
+	google.maps.event.addListener(farm_main_plot_polygon.getPath(), "remove_at", getPolygonCoords);
+	google.maps.event.addListener(farm_main_plot_polygon.getPath(), "set_at", getPolygonCoords);
 }
+
+function getPolygonCoords() {
+	var len = farm_main_plot_polygon.getPath().getLength();
+	var points = [];
+	for (var i = 0; i < len; i++) {
+		point = farm_main_plot_polygon.getPath().getAt(i).toUrlValue(7).split(',');
+		points.push({ lat: point[0], lng: point[1] });
+	}
+	console.log(points);
+}
+
 
 window.initMap = initMap;
